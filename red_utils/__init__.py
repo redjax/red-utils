7bef42a29e8226bdde340710ff7a4d13b9f54514
redjax <none@none.com>
2023-07-16 22:08:40 -0400

    Fix utils, update .gitignore:
    Add try/except for importing fastapi utils.
    
    Fix imports in all __init__.py files.
    
    Add new operations to msgpack_utils. Add serialize/deserialize simple bytestring.
    
    Fix diskcache always creating a cache by adding get_cache() function and a default cache conf dict.

--- red_utils/__init__.py
+++ red_utils/__init__.py
@@ -4,3 +4,9 @@ from . import diskcache_utils
 from . import httpx_utils
 from . import loguru_utils
 from . import msgpack_utils
+
+## Only import fastapi utils if fastapi and uvicorn are installed
+try:
+    from . import fastapi_utils
+except:
+    pass
