384275b8c6e91ff55db5db2f16cb7d746376e961
Jack <redjax@users.noreply.github.com>
2023-07-19 00:53:29 -0400

    Fix/code structure (#120)
    * Update README:
    
    Add description for context managers.
    
    Add install instructions.
    
    * Update README:
    
    Add description for context managers.
    
    Add install instructions.
    
    * Feat/context mgrs (#113)
    
    * Update README:
    
    Add description for context managers.
    
    Add install instructions.
    
    * Revert "Update README:" (#110)
    
    This reverts commit e1743691af1a84aa1d992fb662b9b93685f48b1c.
    
    * Update README: (#111)
    
    Add description for context managers.
    
    Add install instructions.
    
    Co-authored-by: redjax <none@none.com>
    
    ---------
    
    Co-authored-by: redjax <none@none.com>
    
    * Feat/context mgrs (#114)
    
    * Update README:
    
    Add description for context managers.
    
    Add install instructions.
    
    * Revert "Update README:" (#110)
    
    This reverts commit e1743691af1a84aa1d992fb662b9b93685f48b1c.
    
    * Update README: (#111)
    
    Add description for context managers.
    
    Add install instructions.
    
    Co-authored-by: redjax <none@none.com>
    
    ---------
    
    Co-authored-by: redjax <none@none.com>
    
    * Feat/context mgrs (#115)
    
    * Update README:
    
    Add description for context managers.
    
    Add install instructions.
    
    * Revert "Update README:" (#110)
    
    This reverts commit e1743691af1a84aa1d992fb662b9b93685f48b1c.
    
    * Update README: (#111)
    
    Add description for context managers.
    
    Add install instructions.
    
    Co-authored-by: redjax <none@none.com>
    
    * Add context managers:
    
    Add protect module, with ProtectList and ProtectDict.
    
    Add fn_benchmarks module, with benchmark() and async_benchmark()
    
    ---------
    
    Co-authored-by: redjax <none@none.com>
    
    * Feat/context mgrs (#116)
    
    * Update README:
    
    Add description for context managers.
    
    Add install instructions.
    
    * Revert "Update README:" (#110)
    
    This reverts commit e1743691af1a84aa1d992fb662b9b93685f48b1c.
    
    * Update README: (#111)
    
    Add description for context managers.
    
    Add install instructions.
    
    Co-authored-by: redjax <none@none.com>
    
    * Add context managers:
    
    Add protect module, with ProtectList and ProtectDict.
    
    Add fn_benchmarks module, with benchmark() and async_benchmark()
    
    ---------
    
    Co-authored-by: redjax <none@none.com>
    
    * Fixes:
    
    Fix __init__ imports. Add missing imports to red_utils/__init__.py
    
    ---------
    
    Co-authored-by: redjax <none@none.com>

--- red_utils/__init__.py
+++ red_utils/__init__.py
@@ -1,7 +1,29 @@
 from __future__ import annotations
 
-from .context_managers import ListProtect, DictProtect
+## Import modules with only stdblit dependencies directly
+from .context_managers import benchmark, async_benchmark, ListProtect, DictProtect
+from .file_utils import default_json_dir, ts, export_json, crawl_dir
+from .dict_utils import validate_dict, debug_dict, merge_dicts, update_dict
+from .hash_utils import get_hash_from_str
+from .uuid_utils import (
+    UUIDLength,
+    glob_uuid_lens,
+    gen_uuid,
+    trim_uuid,
+    first_n_chars,
+    get_rand_uuid,
+)
+from .time_utils import (
+    default_format as default_ts_format,
+    twelve_hour_format as ts_twelve_hour_format,
+    datetime_as_dt,
+    datetime_as_str,
+    get_ts,
+)
 
+## Use try/except to import modules with dependencies
+#  Keeps program from crashing when only a single module
+#    is missing a depenency.
 try:
     from . import diskcache_utils
 except:
