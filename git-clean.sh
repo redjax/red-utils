089344063c007e96cc22be601a7327c241c23b28
Jack <redjax@users.noreply.github.com>
2023-07-20 00:48:44 -0400

    Fix/git clean (#154)
    * chore(pyproject): Update pyproject, pdm lock. (#145)
    
    Co-authored-by: redjax <none@none.com>
    
    * Topic/utils (#151)
    
    * chore(pyproject): Update pyproject, pdm lock. (#148)
    
    Co-authored-by: redjax <none@none.com>
    
    * Feat/pre commit (#149)
    
    * chore(pyproject): Update pyproject, pdm lock. (#147)
    
    Co-authored-by: redjax <none@none.com>
    
    * feat(pre-commit): Add pre-commit config
    
    Add hooks for cleaning up code with ruff, black, & file checkers.
    
    File checkers include encoding checks, syntax of TOML & YAML files, & mixed line endings.
    
    ---------
    
    Co-authored-by: redjax <none@none.com>
    
    ---------
    
    Co-authored-by: redjax <none@none.com>
    
    * feat(safeguard): Add prompt to verify user wishes to remove all local files not found on remote.
    
    ---------
    
    Co-authored-by: redjax <none@none.com>

--- git-clean.sh
+++ git-clean.sh
@@ -1,5 +1,43 @@
 #!/bin/bash
 
-echo "Cleaning git tree"
+USE_CHOICE=""
 
-git clean -xfd
+function clean_tree() {
+    git clean -xfd
+}
+
+function warn_user() {
+    echo ""
+    echo "!!!WARNING!!!"
+    echo "Cleaning the git tree will remove *all* local files that are not on the remote."
+    echo "This could lead to removal of files like .env or local dist/ builds."
+    echo ""
+    read -p "Please confirm you wish to remove all local files not found on the remote (Y/N): " usr_choice
+    echo ""
+}
+
+function validate_choice() {
+    case $usr_choice in
+    [Yy])
+        echo "Cleaning git tree"
+        clean_tree
+        ;;
+    [Nn])
+        echo "Cancelling git tree clean"
+        ;;
+    *)
+        echo "Invalid choice: $usr_choice. Exiting."
+        exit 1
+        ;;
+    esac
+}
+
+function main() {
+    clear
+    warn_user
+    validate_choice
+}
+
+main
+
+exit $?
