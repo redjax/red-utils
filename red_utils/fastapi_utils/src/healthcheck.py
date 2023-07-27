ef813b1f638b3c942b214d6563627c3de842b176
redjax <none@none.com>
2023-07-26 23:57:37 -0400

    feat(healthcheck): Improve healtcheck
    Add a jsonable_encoder() to encode healthcheck response.
    
    Switch response from a bare string to a JSONResponse() object, with a status.HTTP_200_OK status and X-HEALTHY header.

--- red_utils/fastapi_utils/src/healthcheck.py
+++ red_utils/fastapi_utils/src/healthcheck.py
@@ -4,13 +4,18 @@ from __future__ import annotations
 import logging
 
 from fastapi import APIRouter, HTTPException, status
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
 
 router = APIRouter(tags=["util"], responses={404: {"description": "Not found"}})
 
 
-## Define filter for pings to /healthy
-#  https://stackoverflow.com/a/70810102
 class EndpointFilter(logging.Filter):
+    """Filter pings to /health.
+
+    https://stackoverflow.com/a/70810102
+    """
+
     def filter(self, record: logging.LogRecord) -> bool:
         return record.args and len(record.args) >= 3 and record.args[2] != "/health"
 
@@ -19,6 +24,16 @@ class EndpointFilter(logging.Filter):
 logging.getLogger("uvicorn.access").addFilter(EndpointFilter())
 
 
-@router.get("/health")
-async def healthy() -> str:
-    return "healthy"
+@router.get("/health", summary="Healthcheck")
+async def healthy() -> JSONResponse:
+    """Respond to healthchecks.
+
+    Endpoint does not take any parameters or request bodies.
+
+    Response: a 200 'OK', with a response body containing 'healthy'.
+    """
+    health: dict = jsonable_encoder({"healthy": True})
+    response = JSONResponse(
+        status_code=status.HTTP_200_OK, headers={"X-HEALTHY": "true"}, content=health
+    )
+    return response
