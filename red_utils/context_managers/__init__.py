a911380e3d67051195b6fa2bd6098f60ae82fe9a
Jack <redjax@users.noreply.github.com>
2023-08-06 21:59:02 -0400

    Feat/sqlite context mgr (#251)
    * feat(sqlite-contextmanager): Add SQLite connection context manager
    
    Defines a Python context manager class to control SQLite database interactions.
    
    * Add imports for new SQLiteConnManager context manager
    
    ---------
    
    Co-authored-by: redjax <none@none.com>

--- red_utils/context_managers/__init__.py
+++ red_utils/context_managers/__init__.py
@@ -2,3 +2,4 @@ from __future__ import annotations
 
 from .benchmarks import async_benchmark, benchmark
 from .object_managers.protect import DictProtect, ListProtect
+from .database_managers.sqlite_managers import SQLiteConnManager
