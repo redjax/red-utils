199d89b5396e981b2511019e371731c2e2b2c41d
Jack <redjax@users.noreply.github.com>
2023-09-20 00:12:28 -0400

    Add dependency groups: (#270)
    all: Install all 3rd party modules that have a red_util
    
        fastapi: Utils for fastapi/uvicorn
    
        http: Utils for http requests. Includes a
        request client, cache, & serialization library
    
    Move all utils out of utils/ dir and into std or ext
        (for stdlib utils and "extended" utils for 3rd party modules)
    
    Co-authored-by: redjax <none@none.com>

--- red_utils/__init__.py
+++ red_utils/__init__.py
@@ -1,12 +1,45 @@
 from __future__ import annotations
-
+import pkgutil
 import sys
 
 sys.path.append(".")
 
-from . import (
-    domain,
-    exc as exc,
-    utils,
-)
+from . import domain
+from . import exc
+from . import std
+from . import ext
+
 from .exc import CustomException
+
+
+
+# pkg_import_map: dict = {"loguru": "red_utils.utils.loguru_utils"}
+
+# def pkgutil_loader(package_import_map: dict = {}) -> None:
+#     """For dependencies with requirements, ensure library is only imported
+#     if namespace package is installed.
+    
+#     Example:
+    
+#         red_utils.utils.sqlalchemy_utils will only be imported if sqlalchemy is installed.
+        
+#     """
+#     if not package_import_map:
+#         return ValueError("Package import mapping is empty, no additional utils will be imported.")
+    
+#     if not isinstance(package_import_map, dict):
+#         return TypeError(f"Invalid type for package_import_map: ({type(package_import_map)}). Must be a dict. No additional utils will be imported.")
+        
+#     for k, v in package_import_map.items():
+#         if not isinstance(k, str) or not isinstance(v, str):
+#             return TypeError(f"""
+#                              Invalid key/value pair in package_import_map, keys and values must both be of type str.
+#                                 Key [{k}]:({type(k)})
+#                                 Value [{v}]:({type(v)}))
+#                                 """
+#                              )
+        
+#         if pkgutil.find_loader(k):
+#             pkgutil.get_importer(v)
+            
+# pkgutil_loader(package_import_map=pkg_import_map)
