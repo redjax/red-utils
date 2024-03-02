87595e7709873d258c0fca04a4e51f29f03e3533
redjax <jackenyon@gmail.com>
2024-03-02 16:08:59 -0500

    Add std.list_utils.
    Add list_utils.select_random_from_list()

---
+++ src/red_utils/std/list_utils/operations.py
@@ -0,0 +1,19 @@
+import typing as t
+import random
+
+from .validators import validate_list
+
+def select_random_from_list(lst: list[t.Any] = None) -> t.Any:
+    """Return a randomly selected item from the list.
+    
+    Params:
+        lst (list): The Python list to select from.
+    
+    Returns:
+        (Any): An object from the original list.
+    """
+    validate_list(lst)
+    
+    rand_index: int = random.randint(0, len(lst) - 1)
+    
+    return lst[rand_index]
