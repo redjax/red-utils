2281d83e4f1e2d626862864a3cfe5776724214e1
redjax <jackenyon@gmail.com>
2024-05-08 01:20:44 -0400

    Remove unused dependencies (arrow).
    Update time_utils tests to remove all arrow references.
    
    Add chardet, hishel dependencies.
    
    Remove tox from CI group.
    
    Remove all mkdocs depdencies from dev dependencies (they exist in the docs group)

--- tests/fixtures/ext/time_fixtures.py
+++ tests/fixtures/ext/time_fixtures.py
@@ -1,10 +1,9 @@
 from __future__ import annotations
 
-import arrow
 import pendulum
 from pytest import fixture
 from red_utils.ext import time_utils
-from red_utils.ext.time_utils import arrow_utils
+
 
 @fixture
 def bad_ts_str() -> str:
@@ -17,18 +16,8 @@ def ts_str() -> str:
 
 
 @fixture
-def arrow_epoch_date() -> arrow.Arrow:
-    return arrow.get("1970-01-01T00:00:00Z")
-
-
-@fixture
 def pendulum_epoch_date() -> pendulum.DateTime:
     return pendulum.parse("1970-01-01T:00:00:00Z")
-
-
-@fixture
-def arrow_now() -> arrow.Arrow:
-    return arrow.now()
 
 
 @fixture
