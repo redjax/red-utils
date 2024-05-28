70cea6b6af0bf8f93fb360e3b4a5d8cf5bf9f755
redjax <jackenyon@gmail.com>
2024-05-28 10:28:53 -0400

    Add cache controller & storage methods to httpx_utils

--- src/red_utils/ext/httpx_utils/__init__.py
+++ src/red_utils/ext/httpx_utils/__init__.py
@@ -6,7 +6,12 @@ sys.path.append(".")
 
 from . import constants, controllers, encoders, operations, transports, validators
 from .constants import default_headers
-from .controllers import HTTPXController
+from .controllers import HTTPXController, HishelCacheClientController
+from .cache_storages import (
+    get_hishel_file_storage,
+    get_hishel_inmemory_storage,
+    get_hishel_sqlite_storage,
+)
 from .operations import get_req_client, make_request, merge_headers, update_headers
 from .transports import get_cache_transport
 from .validators import (
