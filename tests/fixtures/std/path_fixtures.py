8473b5b346ad60d827f30ce828a258ace425d5b6
redjax <no@none.com>
2025-02-17 14:09:48 -0500

    Add uuid utils, pytest fixtures

---
+++ tests/fixtures/std/path_fixtures.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+
+from pathlib import Path
+
+from red_utils.std import path_utils
+
+from pytest import fixture
+
+
+@fixture
+def cwd() -> Path:
+    """Create a fixture of the current working directory.
+
+    Useful for testing path/file functions.
+    """
+    _cwd: Path = Path.cwd()
+
+    return _cwd
+
+
+@fixture
+def file_ts() -> str:
+    _ts = path_utils.file_ts()
+
+    return _ts
