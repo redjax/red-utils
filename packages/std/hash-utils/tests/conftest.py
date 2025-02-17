bfa803d9682d889c741c6c7f7c53fa4043ddf556
redjax <no@none.com>
2025-02-17 13:38:44 -0500

    Continue trying to fix tests

--- packages/std/hash-utils/tests/conftest.py
+++ packages/std/hash-utils/tests/conftest.py
@@ -1,18 +1,18 @@
 from __future__ import annotations
 
+import pytest
+
 from red_utils.std import hash_utils
 
-from pytest import fixture
 
-
-@fixture
+@pytest.fixture
 def str_to_hash() -> str:
     hash_str = "This is a string that will be used for hash_utils tests."
 
     return hash_str
 
 
-@fixture
+@pytest.fixture
 def hashed_str() -> str:
     _hashed = hash_utils.get_hash_from_str(input_str=str_to_hash())
 
