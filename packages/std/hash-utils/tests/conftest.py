096506aef69a107167211e3a370cd3c17fdab31a
redjax <no@none.com>
2025-02-17 13:18:03 -0500

    Add start if dict-utils tests

---
+++ packages/std/hash-utils/tests/conftest.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+
+from red_utils.std import hash_utils
+
+from pytest import fixture
+
+
+@fixture
+def str_to_hash() -> str:
+    hash_str = "This is a string that will be used for hash_utils tests."
+
+    return hash_str
+
+
+@fixture
+def hashed_str() -> str:
+    _hashed = hash_utils.get_hash_from_str(input_str=str_to_hash())
+
+    return _hashed
